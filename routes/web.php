e8d197ab1c73e70cb7d4bc275afd2b19dffcc9e0
SunnyTechie <sunnyict001@gmail.com>
2022-06-10 16:28:45 +0100

    changing route settings

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
