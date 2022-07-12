1ebcba784ea3e717255389f82f0b44f41c214a89
SunnyTechie <sunnyict001@gmail.com>
2022-07-12 16:27:18 +0100

    more fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Models\User;
 use App\Models\Audio;
+use App\Models\Media;
 use App\Models\Video;
 use App\Models\Comment;
-use App\Models\PurchasedBook;
+use App\Models\Membership;
 use Illuminate\Http\Request;
+use App\Models\PurchasedBook;
 use App\Models\ReportedComment;
 
 class HomeController extends Controller
@@ -34,12 +36,14 @@ class HomeController extends Controller
         $totalUsers = User::count();
         //Total number of regstered users today
         $totalUsersToday = User::whereDate('created_at', today())->count();
+        //total number of memberships
+        $totalMemberships = Membership::count();
         //Total number of books
         $totalBooks = Book::count();
         //Total number of videos
-        $totalVideos = Video::count();
+        $totalVideos = Media::where('type', 'video')->count();
         //Total number of Audios
-        $totalAudios = Audio::count();
+        $totalAudios = Media::where('type', 'audio')->count();
         //Total number of admins
         $totalAdmins = User::where('role', 'admin')->count();
         //Total number of comments
@@ -53,7 +57,7 @@ class HomeController extends Controller
         $totalPurchaseByBookId = PurchasedBook::selectRaw('book_id, count(*) as total')->groupBy('book_id')->get();
         //dd($totalPurchaseByBookId);
 
-        return view('home', compact('totalUsers', 'totalUsersToday', 'totalBooks', 'totalVideos', 'totalAudios', 'totalAdmins', 'totalComments', 'totalReportedComments', 'totalPurchaseByBookId'));
+        return view('home', compact('totalUsers', 'totalUsersToday', 'totalBooks', 'totalVideos', 'totalAudios', 'totalAdmins', 'totalComments', 'totalReportedComments', 'totalPurchaseByBookId', 'totalMemberships'));
     }
 
     public function media()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,9 +35,9 @@
             <div class="row">
               <div class="col-8">
                 <div class="numbers">
-                  <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Users</p>
+                  <p class="text-sm mb-0 text-capitalize font-weight-bold">Reg. Members</p>
                   <h5 class="font-weight-bolder mb-0">
-                    {{ $totalUsersToday }}
+                    {{ $totalMemberships }}
                     <span class="text-success text-sm font-weight-bolder">+3%</span>
                   </h5>
                 </div>
